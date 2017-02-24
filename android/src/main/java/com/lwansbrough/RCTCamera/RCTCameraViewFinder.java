ac19a527838eff795ba8946d5d8f1c9060fd137a
Mendy <mendy@talenttribe.me>
2017-02-24 11:36:24 +0200

    video recording in android

---
+++ android/src/main/java/com/lwansbrough/RCTCamera/RCTCameraViewFinder.java
@@ -0,0 +1,312 @@
+/**
+ * Created by Fabrice Armisen (farmisen@gmail.com) on 1/3/16.
+ */
+
+package com.lwansbrough.RCTCamera;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.Camera;
+import android.view.TextureView;
+import android.media.MediaRecorder;
+import java.util.List;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.media.AudioManager;
+import android.media.CamcorderProfile;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.VideoView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+class RCTCameraViewFinder extends TextureView implements TextureView.SurfaceTextureListener {
+    private int _cameraType;
+    private SurfaceTexture _surfaceTexture;
+    private boolean _isStarting;
+    private boolean _isStopping;
+    private Camera _camera;
+    private boolean isVideo;
+
+
+    public MediaRecorder mMediaRecorder = new MediaRecorder();
+    private Uri mUri;
+    private boolean isRecording = false;
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
+    public RCTCameraViewFinder(Context context, int type) {
+        super(context);
+        this.setSurfaceTextureListener(this);
+        this._cameraType = type;
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        _surfaceTexture = surface;
+        startCamera();
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        _surfaceTexture = null;
+        stopCamera();
+        return true;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+    }
+
+      public double getRatio() {
+        int width = RCTCamera.getInstance().getPreviewWidth(this._cameraType);
+        int height = RCTCamera.getInstance().getPreviewHeight(this._cameraType);
+        return ((float) width) / ((float) height);
+    }
+
+    public void setCameraMediaTypeVideo(final boolean isVideo) {
+        this.isVideo = isVideo;
+    }
+
+    public boolean isMediaIsVideo() {
+        return this.isVideo;
+    }
+
+    public void setCameraType(final int type) {
+        if (this._cameraType == type) {
+            return;
+        }
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                stopPreview();
+                _cameraType = type;
+                startPreview();
+            }
+        }).start();
+    }
+
+    public void setCaptureQuality(String captureQuality) {
+        RCTCamera.getInstance().setCaptureQuality(_cameraType, captureQuality);
+    }
+
+    public void setTorchMode(int torchMode) {
+        RCTCamera.getInstance().setTorchMode(_cameraType, torchMode);
+    }
+
+    public void setFlashMode(int flashMode) {
+        RCTCamera.getInstance().setTorchMode(_cameraType, flashMode);
+    }
+
+    private void startPreview() {
+        if (_surfaceTexture != null) {
+            startCamera();
+        }
+    }
+
+    private void stopPreview() {
+        if (_camera != null) {
+            stopCamera();
+        }
+    }
+
+    synchronized private void startCamera() {
+        if (!_isStarting) {
+            _isStarting = true;
+            try {
+                _camera = RCTCamera.getInstance().acquireCameraInstance(_cameraType);
+                Camera.Parameters parameters = _camera.getParameters();
+                // set autofocus
+                List<String> focusModes = parameters.getSupportedFocusModes();
+                if (focusModes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+                    parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+                }
+                // set picture size
+                // defaults to max available size
+                Camera.Size optimalPictureSize = RCTCamera.getInstance().getBestPictureSize(_cameraType, Integer.MAX_VALUE, Integer.MAX_VALUE);
+                parameters.setPictureSize(optimalPictureSize.width, optimalPictureSize.height);
+
+                _camera.setParameters(parameters);
+                _camera.setPreviewTexture(_surfaceTexture);
+                _camera.startPreview();
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+                stopCamera();
+            } finally {
+                _isStarting = false;
+            }
+        }
+    }
+
+    synchronized private void stopCamera() {
+        if (!_isStopping) {
+            _isStopping = true;
+            try {
+                if (_camera != null) {
+                    _camera.stopPreview();
+                    RCTCamera.getInstance().releaseCameraInstance(_cameraType);
+                    _camera = null;
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                _isStopping = false;
+            }
+        }
+    }
+
+    // Mendy video config
+
+    public boolean isRecording() {
+        return isRecording;
+    }
+
+    public String startCapture() {
+        if (isRecording) {
+            // stop recording and release camera
+            mMediaRecorder.stop();  // stop the recording
+            releaseMediaRecorder(); // release the MediaRecorder object
+            _camera.lock();         // take camera access back from MediaRecorder
+
+            // inform the user that recording has stopped
+            //setCaptureButtonText("Capture");
+            isRecording = false;
+            return mUri.toString();
+        } else {
+            // initialize video camera
+            if (prepareVideoRecorder()) {
+                // Camera is available and unlocked, MediaRecorder is prepared,
+                // now you can start recording
+                mMediaRecorder.start();
+
+                // inform the user that recording has started
+                //setCaptureButtonText("Stop");
+                isRecording = true;
+            } else {
+                // prepare didn't work, release the camera
+                releaseMediaRecorder();
+                // inform user
+            }
+            return null;
+        }
+    }
+
+
+    private void releaseMediaRecorder(){
+        if (mMediaRecorder != null) {
+            mMediaRecorder.reset();   // clear recorder configuration
+            mMediaRecorder.release(); // release the recorder object
+            mMediaRecorder = null;
+            _camera.lock();           // lock camera for later use
+        }
+    }
+
+    private void releaseCamera(){
+        if (_camera != null){
+            _camera.release();        // release the camera for other applications
+            _camera = null;
+        }
+    }
+
+    private boolean prepareVideoRecorder(){
+
+        mMediaRecorder = new MediaRecorder();
+
+        if (_cameraType == 1) {
+            mMediaRecorder.setOrientationHint(270);
+        } else {
+            mMediaRecorder.setOrientationHint(90);
+        }
+
+        // Step 1: Unlock and set camera to MediaRecorder
+        _camera.unlock();
+        mMediaRecorder.setCamera(_camera);
+
+        // Step 2: Set sources
+        mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+        // Step 3: Set a CamcorderProfile (requires API Level 8 or higher)
+        mMediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+
+        int typeNumber = _cameraType;
+
+        // Step 4: Set output file
+        File file = getOutputMediaFile(this.isVideo ? MEDIA_TYPE_VIDEO : MEDIA_TYPE_IMAGE); //MEDIA_TYPE_IMAGE
+        mUri = Uri.parse(file.toString());
+        RCTCamera.getInstance().setUri(mUri);
+        mMediaRecorder.setOutputFile(file.toString());
+
+        // Step 5: Set the preview output
+        //mMediaRecorder.setPreviewDisplay(cameraView.getHolder().getSurface());
+
+        // Step 6: Prepare configured MediaRecorder
+        try {
+            mMediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            Log.d("TAG", "IllegalStateException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        } catch (IOException e) {
+            Log.d("TAG", "IOException preparing MediaRecorder: " + e.getMessage());
+            releaseMediaRecorder();
+            return false;
+        }
+        return true;
+    }
+
+    /** Create a File for saving an image or video */
+    private static File getOutputMediaFile(int type){
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                                                                                      Environment.DIRECTORY_DCIM), "Live");
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+        if (! mediaStorageDir.exists()){
+            if (! mediaStorageDir.mkdirs()){
+                Log.d("MyCameraApp", "failed to create directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        if (type == MEDIA_TYPE_IMAGE){
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                                 "IMG_"+ timeStamp + ".jpg");
+        } else if(type == MEDIA_TYPE_VIDEO) {
+            mediaFile = new File(mediaStorageDir.getPath() + File.separator +
+                                 "VID_"+ timeStamp + ".mp4");
+        } else {
+            return null;
+        }
+
+        return mediaFile;
+    }
+}
